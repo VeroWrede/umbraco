34e448eda4cc88e99c614a892a7abad6e7d1c749
Vero Wrede <vwrede@decisionhealth.com>
2018-05-29 15:42:05 -0400

    first commit

---
+++ SnakeGenomeGenerator/SnakeGenomeGenerator.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SnakeGenomeGenerator.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
